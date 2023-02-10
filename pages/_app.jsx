7384d2d31e9c8b56f46c8df2064d6655043131b5
Guruvignesh <vigneshguru274@gmail.com>
2023-02-10 15:37:52 +0530

    add metamask wallet connection with thirdweb

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
-
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+const activeChainId = ChainId.Mainnet;
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<ThirdwebProvider desiredChainId={activeChainId}>
+    <Component {...pageProps} />
+    </ThirdwebProvider>);
 }
 
 export default MyApp
